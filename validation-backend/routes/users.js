3501b19e8c7a7d633478d21dde531afd1a8cc3c2
wi1dcode <magosdk@icloud.com>
2022-08-25 10:18:56 +0200

    post method / backend done

--- validation-backend/routes/users.js
+++ validation-backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const users = require("../users")
+var slugify = require("slugify")
+const { body, validationResult } = require("express-validator")
 const { verify } = require("../middlewares/verify")
 
 app.get("/", (req, res) => {
@@ -10,5 +12,28 @@ app.get("/", (req, res) => {
 app.get("/:slug", verify, (req, res) => {
   res.json(req.users)
 })
+
+app.post(
+  "/",
+  body("name").exists().isLength({ min: 4 }),
+  body("password").exists().isLength({ min: 8 }).isLength({ max: 20 }),
+  body("email").exists().isEmail(),
+  body("city").exists().isIn(["Paris", "Tokyo", "Los Angeles"]),
+  body("profile_picture").optional(),
+  (req, res) => {
+    const { errors } = validationResult(req)
+
+    if (errors.length > 0) {
+      res.status(400).json(errors)
+    } else {
+      const newUser = {
+        ...req.body,
+        slug: slugify(req.body.name),
+      }
+      users.push(newUser)
+      res.status(201).json(newUser)
+    }
+  }
+)
 
 module.exports = app
